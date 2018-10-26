c3c2d7ea46c037b7fdfee607a1c83984cd1621ef
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-10-26 12:59:39 +0100

    Basic step by step routes added

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+require('./routes/step-by-step.js')(router)
 
 module.exports = router

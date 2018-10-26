56c5045f52af003e018610ffdf34d2d6ba86bd8f
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-10-26 13:23:22 +0100

    Moved step-by-step includes

--- app/routes/step-by-step.js
+++ app/routes/step-by-step.js
@@ -1,14 +1,18 @@
 module.exports = function (router) {
   router.get(['/defendant-guidance/step-by-step/overview'], function (req, res) {
+    var step = req.query.step || 4.1
     res.render('defendant-guidance/index', {
       stepByStep: true,
-      stepByStepSubPage: true
+      stepByStepSubPage: true,
+      step: step
     })
   })
   router.get(['/defendant-guidance/step-by-step/:page'], function (req, res) {
+    var step = req.query.step
     res.render('defendant-guidance/' + req.params.page, {
       stepByStep: true,
-      stepByStepSubPage: true
+      stepByStepSubPage: true,
+      step: step
     })
   })
 

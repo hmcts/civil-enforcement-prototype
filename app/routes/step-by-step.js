ac1a3dfd302d6bbe748e1b0e65b0f912620efef4
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-10-26 14:46:37 +0100

    Navigation made consistent for step by step work

--- app/routes/step-by-step.js
+++ app/routes/step-by-step.js
@@ -1,14 +1,17 @@
 module.exports = function (router) {
   router.get(['/defendant-guidance/step-by-step/overview'], function (req, res) {
     var step = req.query.step || 4.1
+    var stepCat = step.toString().substr(0,1)
+    var stepSubCat = step.toString().slice(2)
     res.render('defendant-guidance/index', {
       stepByStep: true,
       stepByStepSubPage: true,
-      step: step
+      stepCat: stepCat,
+      stepSubCat: stepSubCat
     })
   })
   router.get(['/defendant-guidance/step-by-step/:page'], function (req, res) {
-    var step = req.query.step
+    var step = req.query.step || 0.0
     var stepCat = step.toString().substr(0,1)
     var stepSubCat = step.toString().slice(2)
     res.render('defendant-guidance/' + req.params.page, {

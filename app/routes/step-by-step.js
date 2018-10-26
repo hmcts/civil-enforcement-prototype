53c315bfe02ecf594eeb712ce3415be38325316f
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-10-26 13:17:12 +0100

    Conditional breadcrumbs added

--- app/routes/step-by-step.js
+++ app/routes/step-by-step.js
@@ -1,8 +1,15 @@
 module.exports = function (router) {
+  router.get(['/defendant-guidance/step-by-step/overview'], function (req, res) {
+    res.render('defendant-guidance/index', {
+      stepByStep: true,
+      stepByStepSubPage: true
+    })
+  })
   router.get(['/defendant-guidance/step-by-step/:page'], function (req, res) {
     res.render('defendant-guidance/' + req.params.page, {
       stepByStep: true,
       stepByStepSubPage: true
     })
   })
+
 }

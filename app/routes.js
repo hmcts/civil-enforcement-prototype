7c26f41fa35005a91eef9c0f72a02288b88ebe33
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-11-02 11:39:14 +0000

    Crude enforcement selection options added

--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 require('./routes/step-by-step.js')(router)
+require('./routes/enforcement-selection.js')(router)
 
 module.exports = router
---
+++ app/routes/enforcement-selection.js
@@ -0,0 +1,31 @@
+module.exports = function (router) {
+  router.post(['/enforcement-selection/', '/enforcement-selection/basic', '/enforcement-selection/bailiffs'], function (req, res) {
+    var redirect
+    switch (req.body['enforcement-type']) {
+      case 'bailiff' :
+        redirect = '/enforcement-selection/bailiffs'
+        break
+      case 'attachment-of-earnings' :
+        redirect = '/claimant-guidance/get-money-deducted-from-wages'
+        break
+      case 'third-party-debt-order' :
+        redirect = '/claimant-guidance/freeze-assets-or-money-in-account'
+        break
+      case 'charging-order' :
+        redirect = '/claimant-guidance/charge-debtors-land-or-property'
+        break
+      case 'order-to-obtain-information-on-finances' :
+        redirect = '/claimant-guidance/order-to-obtain-information-on-finances'
+        break
+      case 'warrant-of-control' :
+        redirect = '/claimant-guidance/send-bailiffs-to-collect-payment#warrant'
+        break
+      case 'writ-of-control' :
+        redirect = '/claimant-guidance/send-bailiffs-to-collect-payment#writ'
+        break
+      default :
+        redirect = '/enforcement-selection/bailiffs'
+    }
+    res.redirect(redirect)
+  })
+}

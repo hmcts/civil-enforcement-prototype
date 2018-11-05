4fabd8eea3718be5f7710de6ed443ddc7ffb814d
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-11-05 15:17:21 +0000

    Basic GOV.UK Pay flow added

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 require('./routes/step-by-step.js')(router)
 require('./routes/enforcement-selection.js')(router)
 require('./routes/writs.js')(router)
+require('./routes/govuk-pay.js')(router)
 
 module.exports = router

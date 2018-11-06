eb8d719a6efe67e9770a7d044f0d639ea71c2f6c
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-11-06 08:08:10 +0000

    Additions to case status page - everything up to serving writ

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,9 +2,11 @@ module.exports = function (router) {
   router.get(['/dashboard/case-alt'], function (req, res) {
     var enforcement = req.query.enforcement || false
     var writApproved = req.query.writApproved || false
+    var writReady = req.query.writReady || false
     res.render('dashboard/case-alt.html', {
       enforcement: enforcement,
-      writApproved: writApproved
+      writApproved: writApproved,
+      writReady: writReady
     })
   })
 }

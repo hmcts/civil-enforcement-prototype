9156369ec4b5ca398a7e9c6eb9498b08eb01491e
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-11-09 12:02:43 +0000

    Basic Dashboard variants added

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,12 +1,31 @@
 module.exports = function (router) {
   router.get(['/dashboard/case-alt'], function (req, res) {
+    var defendant = req.query.defendant || 'Andrew Smith'
     var enforcement = req.query.enforcement || false
     var writApproved = req.query.writApproved || false
     var writReady = req.query.writReady || false
     res.render('dashboard/case-alt.html', {
+      defendant: defendant,
       enforcement: enforcement,
       writApproved: writApproved,
       writReady: writReady
+    })
+  })
+
+  router.get(['/dashboard/case'], function (req, res) {
+    var defendant = req.query.defendant || 'Andrew Smith'
+    var CCJrequested = req.query.CCJrequested || false
+    var CCJapproved = req.query.CCJapproved || false
+    var writApproved = req.query.writApproved || false
+    var writReady = req.query.writReady || false
+    var writServed = req.query.writServed || false
+    res.render('dashboard/case.html', {
+      defendant: defendant,
+      CCJrequested: CCJrequested,
+      CCJapproved: CCJapproved,
+      writApproved: writApproved,
+      writReady: writReady,
+      writServed: writServed
     })
   })
 }

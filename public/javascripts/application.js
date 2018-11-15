53bfdab0a0427734ae9ac774c8044a7030d0d88a
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-11-15 15:30:29 +0000

    Updates to timePassing function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,26 +5,7 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
-function waitAndRedirect (page, waitTime) {
-  var theWaitTime = waitTime || 3000
-  $('#triggerDaysPassing').addClass('active')
-  $('#triggerDaysPassing').on('click', function () {
-    $('#daysPassing').addClass('active')
-    $('#triggerDaysPassing').removeClass('active')
-    for (var i = 0; i <= days; ++i) {
-      setTimeout((function (x) {
-        return function () {
-          $('#days').html(x)
-        }
-      })(i), i * 1000)
-    }
-    setTimeout(function () { document.location.href = page }, theWaitTime)
-    return false
-  })
-}
-
 function daysPass (page, waitTime, days) {
-  var theDays = days || 5
   var theWaitTime = waitTime || 3000
   setTimeout(function () {
     $('#triggerDaysPassing').addClass('active')
@@ -39,7 +20,7 @@ function daysPass (page, waitTime, days) {
         }
       })(i), i * 1000)
     }
-    setTimeout(function () { document.location.href = page }, theWaitTime)
+    setTimeout(function () { document.location.href = page }, (days * 1000))
   })
 }
 
@@ -48,10 +29,6 @@ if ($('#daysPassing').length === 1) {
   var timeToPrompt = ($('#daysPassing').attr('data-dayspasstimetoprompt')) || 5000
   var days = ($('#daysPassing').attr('data-dayspassdays')) || 5
   daysPass(url, timeToPrompt, days)
-}
-
-if ($('#CCJrequested').length === 1) {
-  daysPass('/notifications/claimant/email2?phoneAlert=true', 5000, 5)
 }
 
 if ($('#phone-mockup').length === 1) {

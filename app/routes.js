a2edb1db8024c7dd50ed1aa87bd6f8af39c6917a
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-11-22 13:04:39 +0000

    Defendant dashboard taking shape

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// V1 work (Alpha S4)
 require('./routes/v1/step-by-step.js')(router)
 require('./routes/v1/notifications.js')(router)
 require('./routes/v1/enforcement-selection.js')(router)
 require('./routes/v1/writs.js')(router)
 require('./routes/v1/dashboard.js')(router)
 require('./routes/v1/govuk-pay.js')(router)
+// V2 work (Alpha S5)
+require('./routes/v2/step-by-step.js')(router)
+require('./routes/v2/notifications.js')(router)
+require('./routes/v2/enforcement-selection.js')(router)
+require('./routes/v2/writs.js')(router)
+require('./routes/v2/dashboard.js')(router)
+require('./routes/v2/govuk-pay.js')(router)
 
 module.exports = router
--- app/routes/v2/dashboard.js
+++ app/routes/v2/dashboard.js
@@ -1,12 +1,38 @@
 module.exports = function (router) {
   const version = 'v2'
-  router.get(['/' + version + '/dashboard/'], function (req, res) {
+  const defendantName = 'Andrew Lions'
+  const claimantName = 'Sam Jones'
+
+  router.get(['/' + version + '/dashboard/case', '/' + version + '/dashboard/:dashboardType/case'], function (req, res) {
+    let defendant = req.query.defendant || defendantName
+    let claimant = req.query.claimant || claimantName
+    let simulateTimePassing = req.query.simulateTimePassing || false
+    let CCJrequested = req.query.CCJrequested || false
+    let CCJapproved = req.query.CCJapproved || false
+    let writApproved = req.query.writApproved || false
+    let writReady = req.query.writReady || false
+    let writServed = req.query.writServed || false
+    let dashboardType = req.params.dashboardType || 'claimant'
+    res.render(version + '/dashboard/case.html', {
+      defendant: defendant,
+      claimant: claimant,
+      simulateTimePassing: simulateTimePassing,
+      CCJrequested: CCJrequested,
+      CCJapproved: CCJapproved,
+      writApproved: writApproved,
+      writReady: writReady,
+      writServed: writServed,
+      dashboardType: dashboardType
+    })
+  })
+
+  router.get(['/' + version + '/dashboard/', '/' + version + '/dashboard/:dashboardType'], function (req, res) {
     res.render(version + '/dashboard/index.html', {
     })
   })
 
   router.get(['/' + version + '/dashboard/case-alt'], function (req, res) {
-    let defendant = req.query.defendant || 'Andrew Smith'
+    let defendant = req.query.defendant || defendantName
     let enforcement = req.query.enforcement || false
     let writApproved = req.query.writApproved || false
     let writReady = req.query.writReady || false
@@ -18,22 +44,4 @@ module.exports = function (router) {
     })
   })
 
-  router.get(['/' + version + '/dashboard/case'], function (req, res) {
-    let defendant = req.query.defendant || 'Andrew Smith'
-    let simulateTimePassing = req.query.simulateTimePassing || false
-    let CCJrequested = req.query.CCJrequested || false
-    let CCJapproved = req.query.CCJapproved || false
-    let writApproved = req.query.writApproved || false
-    let writReady = req.query.writReady || false
-    let writServed = req.query.writServed || false
-    res.render(version + '/dashboard/case.html', {
-      defendant: defendant,
-      simulateTimePassing: simulateTimePassing,
-      CCJrequested: CCJrequested,
-      CCJapproved: CCJapproved,
-      writApproved: writApproved,
-      writReady: writReady,
-      writServed: writServed
-    })
-  })
 }

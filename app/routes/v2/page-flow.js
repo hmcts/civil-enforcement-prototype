377b5821424964d376e8180c0913406e0480af0d
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-11-30 09:36:41 +0000

    Progress reading CSV file

--- app/routes/v2/page-flow.js
+++ app/routes/v2/page-flow.js
@@ -1,14 +1,24 @@
 const settings = require('./config')
+const csvtojson = require('csvtojson')
 const pageFlow = require('./pages.json')
+const fs = require('fs')
+const csvFile = './app/views/v2/lab-notes.csv'
 
 module.exports = function (router) {
   const version = 'v2'
   const sprint = 5
+
   router.get(['/' + version + '/page-flow/'], function (req, res) {
+    // let pathToCSV = fs.readFileSync(csvFile, 'utf8')
+    let csvData = csvtojson().fromFile(csvFile).then((jsonObj) => {
+      return jsonObj
+    })
+
     res.render(version + '/page-flow.html',
       {
         pageFlow: pageFlow,
-        sprint: sprint
+        sprint: sprint,
+        csvData: csvData
       }
     )
   })
